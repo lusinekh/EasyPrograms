3b1f4195c4cc3f3e1c239de81d0e7ba065cb7320
User <User@DESKTOP-I87ABNR>
2019-02-05 16:04:45 -0800

    j

---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,512 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //    Random rnd = new Random();
+
+            //int[][,][] a = new int[3][,][]
+            //{
+            //        new int[2,2][]
+            //        {
+            //            { new int[2] { 5, 8 }, new int[2] { 5, 8 } },
+            //            { new int[2] { 5, 8 }, new int[2] { 5, 8 } }
+            //        },
+            //new int[3, 3][]
+            // {
+            //            { new int[3] { 5, 8 ,3}, new int[2] { 5, 8 },new int[2] { 5, 8 } },
+            //            { new int[2] { 5, 8 }, new int[2] { 5, 8 },new int[2] { 5, 8 } },
+            //            { new int[2] { 5, 8 }, new int[2] { 5, 8 },new int[2] { 5, 8 } }
+            // },
+            //        new int[2,2][]
+            //        {
+            //            { new int[2] { 5, 8 }, new int[2] { 5, 8 } },
+            //            { new int[2] { 5, 8 }, new int[2] { 5, 8 } }
+            //        }
+
+            //};
+
+
+
+
+            /////////////////////////inicaziliacia dzerqov
+
+
+            int[,][,][,] n = new int[3, 2][,][,]
+
+            {
+
+
+
+
+
+               {
+                         new int[3, 2][,]
+                       {
+                             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+                              { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+                             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+
+
+                        },
+
+                       new int[3, 2][,]
+                       {
+                             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+                              { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+                             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+
+
+                        }
+
+
+
+                },
+               ////////////////////////////////////////////////////
+               ////////////////////////////////////////////////////
+
+                {
+                         new int[3, 2][,]
+                       {
+                             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+                              { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+                             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+
+
+                        },
+
+                       new int[3, 2][,]
+                       {
+                             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+                              { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+                             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+
+
+                        }
+
+
+
+                },
+
+                ///////////////////////////////////////////////////////////////
+                ///////////////////////////////////////////////////////////////
+
+                {
+                         new int[3, 2][,]
+                       {
+                             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+                              { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+                             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+
+
+                        },
+
+
+                        new int[3, 2][,]
+                       {
+                             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+                              { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+                             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+
+
+                        }
+
+
+                }
+                /////////////////////////////////////////////////////////////////
+                //////////////////////////////////////////////////////////////////
+                ///////////////////////////////////////////////////////////////
+
+
+            };
+
+
+
+
+
+            /////////////////////////////inicazilaciadzerqov
+
+
+
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+
+            //int[,][,][,] b1 = new int[a, b][, ][, ];
+
+            //Random be = new Random();
+
+
+            ////////////////////////////////
+            /////////////////////////////////
+
+            //for(int i=0;i<b1.GetLength(0);i++)
+            //{
+            //    for(int j=0;j<b1.GetLength(1);j++)
+            //        {
+
+
+
+
+            //        for (int e=0;e<b1.GetLength(3);e++)
+
+            //        {
+            //            for (int pe = 0; pe < b1.GetLength(3); pe++)
+
+            //            {
+            //               for(int z=0;z<b1[i,j][e,pe].GetLength(0); z++)
+
+            //                {
+
+            //                    for (int s = 0; s < b1[i, j][e, pe].GetLength(1); s++)
+
+            //                    {
+            //                        b1[i, j][e, pe][z, b] = be.Next(-99, 99);
+            //                        Console.WriteLine(b1[i, j][e, pe][z, b]);
+
+
+
+
+
+
+
+            //                    }
+
+
+
+
+
+
+
+            //                }
+
+
+
+
+
+
+
+            //                }
+
+
+
+
+
+
+
+            //            }
+
+
+
+
+
+
+            //        }  
+
+
+
+
+
+            //        }
+
+
+
+
+
+
+
+            //}
+
+
+
+
+
+
+
+
+            ///////////////////////////////
+            //////////////////////////////
+
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int[,][,][,] b1 = new int[a, b][,][,];
+
+            Random be = new Random();
+
+
+            Console.WriteLine(b1[1, 1][1, 1][1, 1]);
+
+
+            //for (int i = 0; i < b1.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < b1.GetLength(1); j++)
+            //    {
+
+
+
+
+            //        for (int e = 0; e < a; e++)
+
+            //        {
+
+                        
+            //            for (int pe = 0; pe <b+1; pe++)
+
+            //            {
+            //                for (int z = 0; z < a; z++)
+
+            //                {
+
+            //                    for (int s = 0; s < (a*b)+1; s++)
+
+            //                    {
+            //                        b1[i, j][e, pe][z, b] = be.Next(-99, 99);
+            //                        Console.WriteLine(b1[i, j][e, pe][z, b]);
+
+
+
+
+
+
+
+            //                    }
+
+
+
+
+
+
+
+            //                }
+
+
+
+
+
+
+
+            //            }
+
+
+
+
+
+
+
+            //        }
+
+
+
+
+
+
+            //    }
+
+
+
+
+
+            //}
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int[,][,][] a =new int[1, 1][,][]
+        // {
+
+
+
+        //     new int[3][,][]
+        //{
+        //         new int[2,2][]
+        //         {
+        //             { new int[2] { 5, 8 }, new int[2] { 5, 8 } },
+        //             { new int[2] { 5, 8 }, new int[2] { 5, 8 } }
+        //         },
+        // new int[3, 3][]
+        //  {
+        //             { new int[3] { 5, 8 ,3}, new int[2] { 5, 8 },new int[2] { 5, 8 } },
+        //             { new int[2] { 5, 8 }, new int[2] { 5, 8 },new int[2] { 5, 8 } },
+        //             { new int[2] { 5, 8 }, new int[2] { 5, 8 },new int[2] { 5, 8 } }
+        //  },
+        //         new int[2,2][]
+        //         {
+        //             { new int[2] { 5, 8 }, new int[2] { 5, 8 } },
+        //             { new int[2] { 5, 8 }, new int[2] { 5, 8 } }
+        //         }
+        //},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //    for (int i = 0; i < a.Length; i++)
+        //    {
+        //        Console.ForegroundColor = (ConsoleColor)rnd.Next(0, 16);
+        //        for (int j = 0; j < a[i].GetLength(0); j++)
+        //        {
+        //            for (int k = 0; k < a[i].GetLength(1); k++)
+        //            {
+        //                for (int n = 0; n < a[i][j, k].Length; n++)
+        //                {
+        //                    Console.Write(a[i][j, k][n] + "\t");
+        //                }
+        //                Console.WriteLine();
+        //            }
+        //        }
+        //        Console.WriteLine();
+        //    }
+
+        //    Console.ResetColor();
+
+
+
+
+
+        //var bye = new int[3, 2][,]
+        //           {
+        //            { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+        //             { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+        //              { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+
+        //           };
+
+
+
+        //var bf = new int[1, 1][,][,];
+
+
+
+
+
+
+        //    {
+
+        //      new int[3, 2][,]
+        //               {
+        //                { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+        //                 { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+        //                  { new int[2,2] { { 5, 8 }, {8,9 } }, new int[2,3] { { 5, 8,8 }, {8,6,7 } } },
+
+        //               },
+
+
+
+
+
+
+        //};
+
+
+
+
+
+
+
+
+        //int i = int.Parse(Console.ReadLine());
+        //int j = int.Parse(Console.ReadLine());
+        //int[,][,][,] b1 = new int[2, 3][,][,]
+        //{
+
+        //   new int [2,1][,][,]
+        //   {
+
+
+
+        //   }
+
+
+
+
+
+        //};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    }
+    
+
